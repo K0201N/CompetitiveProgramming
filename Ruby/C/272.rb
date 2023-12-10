c2f0cba34daa3be86b91a69d14bbeb99b997e9a4
Kento Nakamura <61852433+K0201N@users.noreply.github.com>
2023-12-10 17:53:05 +0900

    Ruby: C272

--- Ruby/C/272.rb
+++ Ruby/C/272.rb
@@ -1,6 +1,11 @@
 gets
 a = gets.split.map(&:to_i)
-sums = a.combination(2).to_a.map(&:sum)
-evens = sums.select(&:even?)
 
-puts evens.empty? ? '-1' : evens.max
+evens0_odds1 = a.partition(&:even?)
+evens = evens0_odds1[0].max(2)
+odds = evens0_odds1[1].max(2)
+
+e = evens.size == 2 ? evens.sum : -1
+o = odds.size == 2 ? odds.sum : -1
+
+puts [e, o].max
