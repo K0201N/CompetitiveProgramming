624f6397b47c6c384f7094829a241df88b997aaa
Kento Nakamura <61852433+K0201N@users.noreply.github.com>
2024-02-24 18:31:46 +0900

    Rust build

---
+++ Rust/target/debug/deps/1-19f39a65c66bb40f.d
@@ -0,0 +1,5 @@
+/Users/k/Atcoder/Rust/target/debug/deps/lib1-19f39a65c66bb40f.rmeta: src/bin/1.rs
+
+/Users/k/Atcoder/Rust/target/debug/deps/1-19f39a65c66bb40f.d: src/bin/1.rs
+
+src/bin/1.rs:
---
+++ Rust/target/debug/deps/rust-2bd5f6b5686f8b74.d
@@ -0,0 +1,5 @@
+/Users/k/Atcoder/Rust/target/debug/deps/rust-2bd5f6b5686f8b74: src/main.rs
+
+/Users/k/Atcoder/Rust/target/debug/deps/rust-2bd5f6b5686f8b74.d: src/main.rs
+
+src/main.rs:
