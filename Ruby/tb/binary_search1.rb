2090accc176bc881e7159d9d687e1788e60643c2
Kento Nakamura <61852433+K0201N@users.noreply.github.com>
2025-08-05 13:00:10 +0900

    add binary_search

---
+++ Ruby/tb/binary_search1.rb
@@ -0,0 +1,20 @@
+def binary_search(arr, target)
+  left = 0
+  right = arr.size - 1
+
+  while left <= right
+    mid = (left + right) / 2
+    return mid + 1 if arr[mid] == target
+
+    if arr[mid] < target
+      left = mid + 1
+    else
+      right = mid - 1
+    end
+  end
+end
+
+_n, target = gets.split.map(&:to_i)
+arr = gets.split.map(&:to_i)
+
+puts binary_search(arr, target)
